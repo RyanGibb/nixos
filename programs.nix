90adbcadec2e2d02c9480124e4700fc9ce58c80d
Ryan Gibb <ryan@gibbr.org>
2022-03-02 17:55:33 +0000

    update

--- programs.nix
+++ programs.nix
@@ -43,6 +43,7 @@
     };
     promptInit = ''
       PROMPT='%(?..%F{red}%3?%f )%D{%I:%M:%S%p} %F{blue}%n@%m%f:%F{cyan}%~%f%<<''${vcs_info_msg_0_}'$'\n'" %# "
+      # workaround for https://github.com/NixOS/nixpkgs/pull/161701
       export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion history)
     '';
     interactiveShellInit =builtins.readFile ./zsh.cfg;
