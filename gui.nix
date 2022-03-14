83167cc63b59a2a1549d23c6a6eb90ed74bdaef4
Ryan Gibb <ryangibb321@gmail.com>
2022-03-14 17:23:59 +0000

    update

---
+++ gui.nix
@@ -0,0 +1,131 @@
+{ pkgs, ... }:
+
+{
+  networking.networkmanager.enable = true;
+  programs.nm-applet = {
+    enable = true;
+    indicator = true;
+  };
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "uk";
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    users.ryan = import ./home.nix;
+  };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (
+      builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz"
+    ) { inherit pkgs; };
+  };
+
+  environment.systemPackages = with pkgs; [
+    firefox-wayland
+    chromium
+    thunderbird-wayland
+    element-desktop
+    signal-desktop
+    zotero
+    obsidian
+    spotify
+    gparted
+    vscodium
+    vlc
+    gimp
+    go
+    texlive.combined.scheme-full
+    evince
+    pdfpc
+    calibre
+    transmission
+    transmission-gtk
+    drive
+    libreoffice
+  ];
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      jq
+      swaylock
+      swayidle
+      wtype
+      playerctl
+      brightnessctl
+      xdg-utils
+      gammastep
+      waybar
+      alacritty
+      mako
+      wofi
+      gnome.zenity
+      feh
+      copyq
+      gnome.networkmanagerapplet
+      wdisplays
+      pavucontrol
+      xfce.thunar
+      xfce.xfconf # Needed to save the preferences
+      xfce.exo # Used by default for `open terminal here`, but can be changed
+      rofimoji
+      # https://discourse.nixos.org/t/sway-wm-configuration-polkit-login-manager/3857/6
+      polkit_gnome
+      wf-recorder
+      grim
+      slurp
+    ];
+  };
+
+  services.pipewire.enable = true;
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+      gtkUsePortal = true;
+    };
+  };
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    (nerdfonts.override { fonts = [ "DroidSansMono" ]; })
+    font-awesome
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-13.6.9"
+  ];
+
+  # thunar
+  services.gvfs = {
+    enable = true;
+    package = pkgs.gvfs;
+  };
+  # thunar thumbnail support for images
+  services.tumbler.enable = true;
+
+  services.geoclue2.enableDemoAgent = true;
+
+  programs.kdeconnect.enable = true;
+
+  system.stateVersion = "21.11";
+}
+
--- laptop.nix
+++ laptop.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./common.nix
+    ./gui.nix
     <home-manager/nixos> 
   ];
 
@@ -12,27 +13,9 @@
     device = "nodev";
     efiSupport = true;
     useOSProber = true;
-    extraEntries = ''
-      menuentry 'Arch Linux' {
-        configfile (hd0,7)/boot/grub/grub.cfg
-      }
-    '';
   };
 
   networking.hostName = "dell-xps";
-
-  networking.networkmanager.enable = true;
-  programs.nm-applet = {
-    enable = true;
-    indicator = true;
-  };
-
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "uk";
-  };
 
   networking.useDHCP = false;
   networking.interfaces.wlp0s20f3.useDHCP = true;
@@ -48,12 +31,6 @@
     53 # mosh
     631 # printing
   ];
-  
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
 
   # Needed for Keychron K2
   boot.extraModprobeConfig = ''
@@ -75,110 +52,24 @@
     nssmdns = true;
   };
 
-  nixpkgs.config.allowUnfree = true;
-
-  home-manager = {
-    useGlobalPkgs = true;
-    users.ryan = import ./home.nix;
-  };
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (
-      builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz"
-    ) { inherit pkgs; };
-  };
-
   environment.systemPackages = with pkgs; [
-    firefox-wayland
-    chromium
-    thunderbird-wayland
-    element-desktop
-    signal-desktop
-    zotero
-    obsidian
-    spotify
-    gparted
-    vscodium
-    vlc
-    gimp
-    go
-    texlive.combined.scheme-full
-    evince
-    pdfpc
-    calibre
-    transmission
-    transmission-gtk
-    drive
-    libreoffice
+    fusuma
+    kanshi
+    acpi
   ];
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      jq
-      swaylock
-      swayidle
-      fusuma
-      kanshi
-      wtype
-      playerctl
-      brightnessctl
-      xdg-utils
-      gammastep
-      waybar
-      alacritty
-      mako
-      wofi
-      gnome.zenity
-      feh
-      copyq
-      gnome.networkmanagerapplet
-      wdisplays
-      pavucontrol
-      xfce.thunar
-      xfce.xfconf # Needed to save the preferences
-      xfce.exo # Used by default for `open terminal here`, but can be changed
-      rofimoji
-      # https://discourse.nixos.org/t/sway-wm-configuration-polkit-login-manager/3857/6
-      polkit_gnome
-      wf-recorder
-      grim
-    ];
-  };
-
-  services.pipewire.enable = true;
-  xdg = {
-    portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-      ];
-      gtkUsePortal = true;
-    };
-  };
-
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-emoji
-    (nerdfonts.override { fonts = [ "DroidSansMono" ]; })
-    font-awesome
-  ];
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-13.6.9"
-  ];
-
-  # thunar
-  services.gvfs.enable = true; # Mount, trash, and other functionalities
-  services.tumbler.enable = true; # Thumbnail support for images
-
-  services.geoclue2.enableDemoAgent = true;
+  
+  # systemd.user.services.fusuma = {
+  #   enable = true;
+  #   description = "Fusuma touchpad gestures";
+  #   serviceConfig = {
+  #     EnvironmentFile = builtins.toFile "fusuma-environment" "DISPLAY=:0";
+  #     ExecStart = "${pkgs.ruby}/bin/ruby ${pkgs.fusuma}/bin/.fusuma-wrapped";
+  #     Restart = "on-failure";
+  #     RestartSec = "10s";
+  #   };
+  # };
 
   users.users.ryan.extraGroups = [ "input" ];
-
-  programs.kdeconnect.enable = true;
 
   system.stateVersion = "21.11";
 }
