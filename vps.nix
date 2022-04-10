786af2ebe8760f39bd72b1610afe051b1876bb3a
Ryan Gibb <ryan@gibbr.org>
2022-04-10 19:07:02 +0100

    DNSSEC

--- vps.nix
+++ vps.nix
@@ -47,9 +47,14 @@
 
   services.bind = {
     enable = true;
+    extraOptions = ''
+      dnssec-enable yes;
+      dnssec-validation yes;
+      dnssec-lookaside auto;
+    '';
     zones."gibbr.org" = {
       master = true;
-      file = "/etc/nixos/gibbr.org.zone";
+      file = "/etc/nixos/gibbr.org.zone.signed";
       # axfr zone transfer
       slaves = [
         "127.0.0.1"
