5d0c71c336608c886e173cbe1a57704870e198d3
Ryan Gibb <ryan@gibbr.org>
2022-05-30 10:57:12 +0100

    update

--- modules/dns/bind.nix
+++ modules/dns/bind.nix
@@ -1,14 +1,9 @@
 {
   services.bind = {
     enable = true;
-    extraOptions = ''
-      dnssec-enable yes;
-      dnssec-validation yes;
-      dnssec-lookaside auto;
-    '';
     zones."gibbr.org" = {
       master = true;
-      file = "/etc/nixos/dns/gibbr.org.zone.signed";
+      file = "/etc/nixos/modules/dns/gibbr.org.zone.signed";
       # axfr zone transfer
       slaves = [
         "127.0.0.1"
