532a2cc8c3a071e0673e8137c3ab152e6eb8e2ad
Ryan Gibb <ryangibb321@gmail.com>
2022-06-28 22:33:51 +0100

    pin channels

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 
 let
-  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  unstable = import (builtins.fetchTarball {
+    name = "nixos-unstable-2022-05-27";
+    url = "https://github.com/nixos/nixpkgs/archive/5ce6597eca7d7b518c03ecda57d45f9404b5e060.tar.gz";
+    sha256 = "1hs1lnnbp1dky3nfp7xlricpp5c63sr46jyrnvykci8bl8jnxnl3";
+  }) { config = { allowUnfree = true; }; };
 in {
   networking.networkmanager.enable = true;
   programs.nm-applet = {
