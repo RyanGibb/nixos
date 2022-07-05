0d626d3259d906d420269e85d6d07b2577c99433
Ryan Gibb <ryan@gibbr.org>
2022-07-05 12:27:33 +0100

    updates

--- modules/hosts/rasp-pi.nix
+++ modules/hosts/rasp-pi.nix
@@ -22,46 +22,11 @@
     useDHCP = false;
     interfaces.eth0.useDHCP = true;
     interfaces.wlan0.useDHCP = true;
-    firewall = {
-      allowedTCPPorts = lib.mkForce [ 25 53 80 443 465 993 ];
-      allowedUDPPorts = lib.mkForce [ 53 ];
-      trustedInterfaces = [ "tailscale0" ];
-    };
+    firewall.trustedInterfaces = [ "tailscale0" ];
   };
-
-  services.nginx = {
-    enable = true;
-    virtualHosts."gibbr.org" = {
-      forceSSL = true;
-      enableACME = true;
-      root = "/var/www/gibbr.org";
-      extraConfig = ''
-        error_page 403 =404 /404.html;
-        error_page 404 /404.html;
-      '';
-    };
-    virtualHosts."www.gibbr.org" = {
-      addSSL = true;
-      useACMEHost = "gibbr.org";
-      extraConfig = ''
-        return 301 $scheme://gibbr.org$request_uri;
-      '';
-    };
-  };
-
-  security.acme = {
-    defaults.email = "ryan@gibbr.org";
-    acceptTerms = true;
-    certs."gibbr.org".extraDomainNames = [ "www.gibbr.org" ];
-  };
-
-  services.logind.extraConfig = ''
-    RuntimeDirectorySize=1G
-    RuntimeDirectoryInodesMax=402566
-  '';
 
   services.journald.extraConfig = ''
-    SystemMaxUse=1G
+    SystemMaxUse=4G
   '';
 
   nix.autoOptimiseStore = true;
