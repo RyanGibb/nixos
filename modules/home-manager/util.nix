0f5bf68f3826aa0fa62798cab31d21832de98a5d
Ryan Gibb <ryan@gibbr.org>
2022-07-24 13:24:56 +0100

    fix "syntax error, unexpected '/', at modules/home-manager/util.nix:7:72"

--- modules/home-manager/util.nix
+++ modules/home-manager/util.nix
@@ -4,7 +4,7 @@
   listFilesInDir = src: lib.attrsets.mapAttrsToList (name: value: "${src}/${name}") (builtins.readDir src);
   inDirReplace = src: dst: replacements:
     let filenames = lib.attrsets.mapAttrsToList (name: value: "${name}") (builtins.readDir src); in
-    let substitutedSource = file: { source = (pkgs.substituteAll ({src=/${src}/${file}; isExecutable = true;} // replacements)); }; in
+    let substitutedSource = file: { source = (pkgs.substituteAll ({src="/${src}/${file}"; isExecutable = true;} // replacements)); }; in
     let attrs = builtins.map (file: lib.attrsets.nameValuePair "${dst}/${file}" (substitutedSource file)) filenames; in
     builtins.listToAttrs attrs;
   concatFilesReplace = filenames: replacements:
