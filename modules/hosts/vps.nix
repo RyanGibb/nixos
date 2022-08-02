0b3579feb75230d95083dca0b9ea3331442f54a3
Ryan Gibb <ryangibb321@gmail.com>
2022-08-02 17:17:25 +0100

    update

--- modules/hosts/vps.nix
+++ modules/hosts/vps.nix
@@ -37,32 +37,31 @@
   };
   
   networking.firewall = {
-      allowedTCPPorts = lib.mkForce [ 25 53 80 443 465 993 ];
-      allowedUDPPorts = lib.mkForce [ 53 51820 ];
-      trustedInterfaces = [ "tailscale0" ];
-      extraCommands = ''
-        iptables -P FORWARD DROP
+    allowedTCPPorts = lib.mkForce [ 25 53 80 443 465 993 ];
+    allowedUDPPorts = lib.mkForce [ 53 51820 ];
+    trustedInterfaces = [ "tailscale0" ];
+    extraCommands = ''
+      iptables -P FORWARD DROP
 
-        ### proxy HTTP/HTTPS
+      ### proxy HTTP/HTTPS
 
-        #### forward syn packet
-        # iptables -A FORWARD -i enp1s0 -o tailscale0 -p tcp --syn --match multiport --dports 80,443 -m conntrack --ctstate NEW -j ACCEPT
+      #### forward syn packet
+      # iptables -A FORWARD -i enp1s0 -o tailscale0 -p tcp --syn --match multiport --dports 80,443 -m conntrack --ctstate NEW -j ACCEPT
 
-        #### forward packets for established flows bidirectionally
-        # iptables -A FORWARD -i enp1s0 -o tailscale0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-        # iptables -A FORWARD -i tailscale0 -o enp1s0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+      #### forward packets for established flows bidirectionally
+      # iptables -A FORWARD -i enp1s0 -o tailscale0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+      # iptables -A FORWARD -i tailscale0 -o enp1s0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-        #### proxy ports
-        # iptables -t nat -A PREROUTING -i enp1s0 -p tcp --match multiport --dports 80,443 -j DNAT --to-destination 100.92.63.87
-        # iptables -t nat -A POSTROUTING -o tailscale0 -p tcp --match multiport --dports 80,443 -d 100.92.63.87 -j SNAT --to-source 100.125.253.71
+      #### proxy ports
+      # iptables -t nat -A PREROUTING -i enp1s0 -p tcp --match multiport --dports 80,443 -j DNAT --to-destination 100.92.63.87
+      # iptables -t nat -A POSTROUTING -o tailscale0 -p tcp --match multiport --dports 80,443 -d 100.92.63.87 -j SNAT --to-source 100.125.253.71
 
-        ### proxy DNS
-        # iptables -A FORWARD -i enp1s0 -o tailscale0 -p udp -j ACCEPT
-        # iptables -A FORWARD -i tailscale0 -o enp1s0 -p udp -j ACCEPT
-        # iptables -t nat -A PREROUTING -i enp1s0 -p udp --dport 53 -j DNAT --to-destination 100.92.63.87
-        # iptables -t nat -A POSTROUTING -o tailscale0 -p udp --dport 53 -d 100.92.63.87 -j SNAT --to-source 100.125.253.71
-      '';
-    };
+      ### proxy DNS
+      # iptables -A FORWARD -i enp1s0 -o tailscale0 -p udp -j ACCEPT
+      # iptables -A FORWARD -i tailscale0 -o enp1s0 -p udp -j ACCEPT
+      # iptables -t nat -A PREROUTING -i enp1s0 -p udp --dport 53 -j DNAT --to-destination 100.92.63.87
+      # iptables -t nat -A POSTROUTING -o tailscale0 -p udp --dport 53 -d 100.92.63.87 -j SNAT --to-source 100.125.253.71
+    '';
   };
 
   services.nginx = {
