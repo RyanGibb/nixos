fd6b7131c70632d6ca64472f9c4f0ebcab1a7bcf
Ryan Gibb <ryan@gibbr.org>
2022-08-12 11:36:23 +0100

    update

--- modules/dns/bind.nix
+++ modules/dns/bind.nix
@@ -2,6 +2,7 @@
   # TODO make zonefile nix derivation
   services.bind = {
     enable = true;
+    cacheNetworks = [ "0.0.0.0/0" ];
     zones."gibbr.org" = {
       master = true;
       file = "/etc/nixos/modules/dns/gibbr.org.zone";
