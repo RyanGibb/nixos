f739f8f2de7413bfda8a00ef09f34d5eceba3f59
Ryan Gibb <ryan@gibbr.org>
2022-08-31 22:54:35 +0200

    home-manager follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }@inputs: {
