ae1cca645a5d873b92371484d8368c0b757ea07d
Ryan Gibb <ryan@gibbr.org>
2022-09-06 11:52:52 +0200

    use lib.pipe

--- modules/home-manager/util.nix
+++ modules/home-manager/util.nix
@@ -2,11 +2,17 @@
 
 {
   listFilesInDir = src: lib.attrsets.mapAttrsToList (name: value: "${src}/${name}") (builtins.readDir src);
-  inDirReplace = src: dst: replacements:
-    let filenames = lib.attrsets.mapAttrsToList (name: value: "${name}") (builtins.readDir src); in
-    let substitutedSource = file: { source = (pkgs.substituteAll ({src="/${src}/${file}"; isExecutable = true;} // replacements)); }; in
-    let attrs = builtins.map (file: lib.attrsets.nameValuePair "${dst}/${file}" (substitutedSource file)) filenames; in
-    builtins.listToAttrs attrs;
+  inDirReplace = src: dst: replacements: lib.pipe src [
+    # get filenames in src directory
+    (builtins.readDir src)
+    (lib.attrsets.mapAttrsToList (name: value: "${name}"))
+    # call `substituteAll` on all files
+    (
+      let substitutedSource = file: { source = (pkgs.substituteAll ({src="/${src}/${file}"; isExecutable = true;} // replacements)); }; in
+      builtins.map (file: lib.attrsets.nameValuePair "${dst}/${file}" (substitutedSource file))
+    )
+    (builtins.listToAttrs attrs)
+  ];
   concatFilesReplace = filenames: replacements:
     let fromStrings = lib.attrsets.mapAttrsToList (name: value: "@${name}@") replacements; in
     let toStrings =   lib.attrsets.mapAttrsToList (name: value: "${value}") replacements; in
