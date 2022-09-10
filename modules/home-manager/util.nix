5861685377f69f2869420cec1cb5807c6c0646f9
Ryan Gibb <ryan@gibbr.org>
2022-09-11 01:15:26 +0200

    fix lib.pipe usage

--- modules/home-manager/util.nix
+++ modules/home-manager/util.nix
@@ -4,14 +4,14 @@
   listFilesInDir = src: lib.attrsets.mapAttrsToList (name: value: "${src}/${name}") (builtins.readDir src);
   inDirReplace = src: dst: replacements: lib.pipe src [
     # get filenames in src directory
-    (builtins.readDir src)
+    builtins.readDir
     (lib.attrsets.mapAttrsToList (name: value: "${name}"))
     # call `substituteAll` on all files
     (
       let substitutedSource = file: { source = (pkgs.substituteAll ({src="/${src}/${file}"; isExecutable = true;} // replacements)); }; in
       builtins.map (file: lib.attrsets.nameValuePair "${dst}/${file}" (substitutedSource file))
     )
-    (builtins.listToAttrs attrs)
+    builtins.listToAttrs
   ];
   concatFilesReplace = filenames: replacements:
     let fromStrings = lib.attrsets.mapAttrsToList (name: value: "@${name}@") replacements; in
