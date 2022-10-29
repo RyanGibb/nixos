085a817bfe961d958cc0ecb8adf1cf4e30be6d9a
Ryan Gibb <ryangibb321@gmail.com>
2022-10-29 12:54:38 +0100

    obsidian sync

--- scripts/obsidian_sync.sh
+++ scripts/obsidian_sync.sh
@@ -1,5 +1,5 @@
 cd /storage/emulated/0/Documents/vault
-git pull || exit 1
-git add . || exit 1
-git commit -m "phone $(date '+%Y-%m-%d %H.%M')" || exit 1
-git push || exit 1
+git pull &&
+git add . &&
+git commit -m "phone $(date '+%Y-%m-%d %H.%M')" &&
+git push
