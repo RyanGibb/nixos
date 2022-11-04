0287857a9f953974276803726cad6013e1a3c41d
Ryan Gibb <ryan@gibbr.org>
2022-11-04 15:48:36 +0000

    rasp-pi

--- hosts/rasp-pi/default.nix
+++ hosts/rasp-pi/default.nix
@@ -4,11 +4,41 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/common/default.nix
-    ../../modules/../secret/wifi.nix
     ../../modules/services/wireguard/default.nix
+    ../../modules/dns/bind.nix
+    ../../secret/wifi.nix
   ];
 
   machineColour = "red";
+
+  services.tailscale.enable = true;
+
+  boot.loader.grub.enable = false;
+  services.nginx = {
+    enable = true;
+    virtualHosts."gibbr.org" = {
+      #forceSSL = true;
+      #enableACME = true;
+      root = "/var/www/gibbr.org";
+      extraConfig = ''
+        error_page 403 =404 /404.html;
+        error_page 404 /404.html;
+      '';
+    };
+    virtualHosts."www.gibbr.org" = {
+      #addSSL = true;
+      #useACMEHost = "gibbr.org";
+      extraConfig = ''
+        return 301 $scheme://gibbr.org$request_uri;
+      '';
+    };
+  };
+
+  #security.acme = {
+  #  defaults.email = "ryan@gibbr.org";
+  #  acceptTerms = true;
+  #  certs."gibbr.org".extraDomainNames = [ "www.gibbr.org" ];
+  #};
 
   networking.wireless.enable = true;
 
