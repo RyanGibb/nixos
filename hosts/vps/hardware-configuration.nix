5935e8829f3d3d6da9f9c99e2f0f7ba18a859275
Ryan Gibb <ryan@gibbr.org>
2022-11-06 17:04:52 +0000

    update

--- hosts/vps/hardware-configuration.nix
+++ hosts/vps/hardware-configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  ];
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [ ];
@@ -17,5 +17,8 @@
 
   swapDevices = [ ];
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
