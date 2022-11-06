ddb4f499ce170e10c84f089e07c13b6069a57aba
Ryan Gibb <ryangibb321@gmail.com>
2022-11-06 22:48:58 +0000

    for @NPopiel

--- hosts/vps/default.nix
+++ hosts/vps/default.nix
@@ -23,6 +23,10 @@
 
   swapDevices = [ { device = "/var/swap"; size = 2048; } ];
 
+  # I ran into some issues building a MirageOS DNS unikernel on the server as it has a small amount (1GB) of ram.
+  # This isn't an issue normally due to a healthy swapfile and low load, but opam seems to use the in-memory filesystem created by `pam-systemd` at `/run/user/$uid` which by default is limited to 10% of ram, with a number of inodes equal to this divided by 4096 (e.g. 4KB per inode).
+  # The space wasn't an issue but the number of inodes were.
+  # We explicitly set a large number of inodes to fix this.
   services.logind.extraConfig = ''
     RuntimeDirectorySize=1G
     RuntimeDirectoryInodesMax=402566
