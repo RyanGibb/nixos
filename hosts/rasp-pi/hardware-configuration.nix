c4585a8d395e10590ea682a4d2dbc54541fba51c
Ryan Gibb <ryan@gibbr.org>
2022-11-08 18:50:27 +0000

    fix bootloader

--- hosts/rasp-pi/hardware-configuration.nix
+++ hosts/rasp-pi/hardware-configuration.nix
@@ -13,8 +13,9 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
   boot.loader.grub.enable = false;
-  boot.loader.raspberryPi.enable = true;
-  boot.loader.raspberryPi.version = 4;
+  #boot.loader.raspberryPi.enable = true;
+  #boot.loader.raspberryPi.version = 4;
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
