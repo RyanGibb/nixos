1367592273e0b37c4cd0695032fd65c5e7e25e9a
Ryan Gibb <ryan@gibbr.org>
2022-11-09 14:32:08 +0000

    update

--- hosts/rasp-pi/default.nix
+++ hosts/rasp-pi/default.nix
@@ -11,36 +11,11 @@
   machineColour = "red";
 
   services.tailscale.enable = true;
-
-  services.nginx = {
-    enable = true;
-    virtualHosts."gibbr.org" = {
-      #forceSSL = true;
-      #enableACME = true;
-      root = "/var/www/gibbr.org";
-      extraConfig = ''
-        error_page 403 =404 /404.html;
-        error_page 404 /404.html;
-      '';
-    };
-    virtualHosts."www.gibbr.org" = {
-      #addSSL = true;
-      #useACMEHost = "gibbr.org";
-      extraConfig = ''
-        return 301 $scheme://gibbr.org$request_uri;
-      '';
-    };
-  };
-
-  #security.acme = {
-  #  defaults.email = "ryan@gibbr.org";
-  #  acceptTerms = true;
-  #  certs."gibbr.org".extraDomainNames = [ "www.gibbr.org" ];
-  #};
-
   networking.networkmanager.enable = true;
 
   services.journald.extraConfig = ''
     SystemMaxUse=4G
   '';
+
+  services."gibbr.org".enable = true;
 }
