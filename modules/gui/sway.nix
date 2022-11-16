ef81dc4ea4633ade90ce21e26472f189fe8d5313
Ryan Gibb <ryangibb321@gmail.com>
2022-11-16 12:43:56 +0000

    fixed duplicate desktop entries [0]
    [0] `~./.nix-profile/share/applications/*.desktop` and `~/.local/share/applications/home-manager/*.desktop`

--- modules/gui/sway.nix
+++ modules/gui/sway.nix
@@ -7,7 +7,58 @@
   
   home-manager.users.ryan = import ../home-manager/sway.nix;
 
-  programs.sway = {
+  programs.sway =
+  let
+    desktopEntries = [
+      (pkgs.makeDesktopItem {
+        name = "nvim";
+        desktopName = "Neovim";
+        genericName = "Text Editor";
+        exec = "alacritty -e nvim %F";
+        terminal = false;
+        categories = [ "Application" "Utility" "TextEditor" ];
+        icon = "nvim";
+        mimeTypes = [ "text/english" "text/plain" ];
+      })
+      (pkgs.makeDesktopItem {
+        name = "obsidian-wayland";
+        desktopName = "Obsidian (wayland)";
+        exec = "obsidian --ozone-platform=wayland";
+        terminal = false;
+        categories = [ "Office" ];
+        comment = "Knowledge base";
+        icon = "obsidian";
+        type = "Application";
+      })
+      (pkgs.makeDesktopItem {
+        name = "codium-wayland";
+        desktopName = "VSCodium (wayland)";
+        genericName = "Text Editor";
+        exec = "codium --ozone-platform=wayland %F";
+        icon = "code";
+        mimeTypes = [ "text/plain" "inode/directory" ];
+        terminal = false;
+      })
+      (pkgs.makeDesktopItem {
+        name = "chromium-wayland";
+        desktopName = "Chromium (wayland)";
+        exec = "chromium --ozone-platform-hint=auto";
+        icon = "chromium";
+      })
+      (pkgs.makeDesktopItem {
+        name = "signal-desktop-wayland";
+        desktopName = "Signal (wayland)";
+        exec = "signal-desktop --ozone-platform=wayland";
+        terminal = false;
+        type = "Application";
+        icon = "signal-desktop";
+        comment = "Private messaging from your desktop";
+        mimeTypes = [ "x-scheme-handler/sgnl" "x-scheme-handler/signalcaptcha" ];
+        categories = [ "Network" "InstantMessaging" "Chat" ];
+      })
+    ];
+  in
+  {
     enable = true;
     wrapperFeatures.gtk = true; # so that gtk works properly
     extraPackages = with pkgs; [
@@ -29,7 +80,7 @@
       swappy
       mako
       kanshi
-    ];
+    ] ++ desktopEntries;
   };
 
   xdg.portal = {
