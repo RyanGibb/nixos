dd6be3941bcda381ddf7f363ae5ba254ce6c42eb
Ryan Gibb <ryan@gibbr.org>
2022-11-21 12:52:30 +0000

    https://github.com/felx/mastodon-documentation/blob/master/Running-Mastodon/Serving_a_different_domain.md#configuring-domainorg

--- modules/services/mastodon.nix
+++ modules/services/mastodon.nix
@@ -3,9 +3,6 @@
 {
   services.mastodon = {
     enable = true;
-    # used for proxy
-    localDomain = "mastodon.gibbr.org";
-    configureNginx = true;
     enableUnixSocket = false;
     webProcesses = 1;
     webThreads = 3;
@@ -28,6 +25,38 @@
       SMTP_SSL="true";
       SMTP_ENABLE_STARTTLS="false";
       SMTP_OPENSSL_VERIFY_MODE="none";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      # relies on gibbr.org being set up
+      "gibbr.org".locations."/.well-known/host-meta".extraConfig = ''
+        return 301 https://mastodon.gibbr.org$request_uri;
+      '';
+      "mastodon.gibbr.org" = {
+        root = "${config.services.mastodon.package}/public/";
+        forceSSL = true;
+        enableACME = true;
+
+        locations."/system/".alias = "/var/lib/mastodon/public-system/";
+
+        locations."/" = {
+          tryFiles = "$uri @proxy";
+        };
+
+        locations."@proxy" = {
+          proxyPass = "http://127.0.0.1:${builtins.toString config.services.mastodon.webPort}";
+          proxyWebsockets = true;
+        };
+
+        locations."/api/v1/streaming/" = {
+          proxyPass = "http://127.0.0.1:${builtins.toString config.services.mastodon.streamingPort}/";
+          proxyWebsockets = true;
+        };
+      };
     };
   };
 }
