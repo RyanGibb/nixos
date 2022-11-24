d409b776247ea02b1b0d7b6891742d1db7c2eaf6
Ryan Gibb <ryangibb321@gmail.com>
2022-11-24 00:31:42 +0000

    nixified dns zonefile

--- modules/dns/default.nix
+++ modules/dns/default.nix
@@ -45,10 +45,127 @@ let mkOption = lib.mkOption; in
             type = "A";
             data = config.custom.serverIpv4;
           }
-        ]) [ "ns1" "ns2" ]
-        ;# ++ [
-          
-        #]
+        ]) [ "ns1" "ns2" ] ++
+        [
+          {
+            name = "www";
+            type = "A";
+            data = config.custom.serverIpv4;
+          }
+
+          {
+            name = "@";
+            type = "A";
+            data = config.custom.serverIpv4;
+          }
+          {
+            name = "@";
+            type = "AAA";
+            data = config.custom.serverIpv6;
+          }
+          {
+            name = "vps";
+            type = "A";
+            data = config.custom.serverIpv4;
+          }
+          {
+            name = "vps";
+            type = "AAA";
+            data = config.custom.serverIpv6;
+          }
+          {
+            name = "twitcher";
+            type = "CNAME";
+            data = "vps";
+          }
+          {
+            name = "git";
+            type = "CNAME";
+            data = "vps";
+          }
+          {
+            name = "mastodon";
+            type = "CNAME";
+            data = "vps";
+          }
+
+          {
+            name = "mail";
+            type = "A";
+            data = config.custom.serverIpv4;
+          }
+          {
+            name = "mail";
+            type = "AAAA";
+            data = config.custom.serverIpv6;
+          }
+          {
+            name = "@";
+            type = "MX";
+            data = "10 mail";
+          }
+          {
+            name = "@";
+            type = "TXT";
+            data = "\"v=spf1 a:mail.${config.networking.domain} -all\"";
+          }
+
+          {
+            name = "mail._domainkey";
+            ttl = 10800;
+            type = "TXT";
+            data = "\"v=DKIM1; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6YmYYvoFF7VqtGcozpVQa78aaGgZdvc5ZIHqzmkKdCBEyDF2FRbCEK4s2AlC8hhc8O4mSSe3S4AzEhlRgHXbU22GBaUZ3s2WHS8JJwZvWeTjsbXQwjN/U7xpkqXPHLH9IVfOJbHlp4HQmCAXw4NaypgkkxIGK0jaZHm2j6/1izQIDAQAB\"";
+          }
+          {
+            name = "_dmarc";
+            ttl = 10800;
+            type = "TXT";
+            data = "\"v=DMARC1; p=none\"";
+          }
+
+          {
+            name = "pixel-4a.vpn";
+            type = "A";
+            data = "100.122.46.94";
+          }
+          {
+            name = "dell-xps.vpn";
+            type = "A";
+            data = "100.92.111.117";
+          }
+          {
+            name = "hp-pavilion.";
+            type = "A";
+            data = "100.91.12.120";
+          }
+          {
+            name = "vps.vpn";
+            type = "A";
+            data = "100.88.115.118";
+          }
+          {
+            name = "desktop.vpn";
+            type = "A";
+            data = "100.93.8.35";
+          }
+          {
+            name = "rasp-pi.vpn";
+            type = "A";
+            data = "100.92.63.87";
+          }
+
+          {
+            name = "@";
+            type = "TXT";
+            data = "google-site-verification=xM7L59imw-53rUPQIWD0VSrAEa94z2WAZ-FccXqX9V0";
+          }
+
+          {
+            name = "@";
+            type = "LOC";
+            data = "52 12 40.4 N 0 5 31.9 E 22m 10m 10m 10m";
+          }
+        ]
     };
   };
 }
--- modules/dns/zonefile.nix
+++ modules/dns/zonefile.nix
@@ -14,35 +14,7 @@ let cfg = config.dns; in pkgs.writeTextFile {
     )
     ${
       lib.strings.concatStringsSep "\n"
-        (builtins.map (rr: "${rr.name} IN ${rr.type} ${rr.data}") cfg.records)
+        (builtins.map (rr: "${rr.name} IN ${builtins.toString rr.ttl} ${rr.type} ${rr.data}") cfg.records)
     }
-
-    www             IN A     ${config.custom.serverIpv4}
-
-    @               IN A     ${config.custom.serverIpv4}
-    @               IN AAAA  ${config.custom.serverIpv6}
-    vps             IN A     ${config.custom.serverIpv4}
-    vps             IN AAAA  ${config.custom.serverIpv6}
-    twitcher        IN CNAME vps
-    git             IN CNAME vps
-    mastodon        IN CNAME vps
-
-    mail            IN A     ${config.custom.serverIpv4}
-    mail            IN AAAA  ${config.custom.serverIpv6}
-    @               IN MX    10 mail
-    @               IN TXT   "v=spf1 a:mail.${config.networking.domain} -all"
-    mail._domainkey IN 10800 TXT "v=DKIM1; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6YmYYvoFF7VqtGcozpVQa78aaGgZdvc5ZIHqzmkKdCBEyDF2FRbCEK4s2AlC8hhc8O4mSSe3S4AzEhlRgHXbU22GBaUZ3s2WHS8JJwZvWeTjsbXQwjN/U7xpkqXPHLH9IVfOJbHlp4HQmCAXw4NaypgkkxIGK0jaZHm2j6/1izQIDAQAB"
-    _dmarc          IN 10800 TXT "v=DMARC1; p=none"
-
-    pixel-4a.vpn    IN A     100.122.46.94
-    dell-xps.vpn    IN A     100.92.111.117
-    hp-pavilion.vpn IN A     100.91.12.120
-    vps.vpn         IN A     100.88.115.118
-    desktop.vpn     IN A     100.93.8.35
-    rasp-pi.vpn     IN A     100.92.63.87
-
-    @               IN TXT   google-site-verification=xM7L59imw-53rUPQIWD0VSrAEa94z2WAZ-FccXqX9V0
-
-    @               IN LOC  52 12 40.4 N 0 5 31.9 E 22m 10m 10m 10m 
   '';
 }
