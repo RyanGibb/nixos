c1c64ccf68a739985a673c090469043b25f6de75
Ryan Gibb <ryan@freumh.org}>
2022-11-25 15:58:54 +0000

    hertzner vps ipv6

--- hosts/vps/hardware-configuration.nix
+++ hosts/vps/hardware-configuration.nix
@@ -6,4 +6,16 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  networking = {
+    interfaces."enp1s0" = {
+      ipv6.addresses = [{
+        address = "2a01:4f9:c011:87ad::";
+        prefixLength = 64;
+      }];
+    };
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp1s0";
+    };
+  };
 }
