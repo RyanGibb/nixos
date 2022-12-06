21341cef371104127c1f63fa9ce39d886ab9844f
Ryan Gibb <ryangibb321@gmail.com>
2022-12-06 14:39:50 +0000

    tailscale ips /etc/hosts entries

--- modules/personal/tailscale.nix
+++ modules/personal/tailscale.nix
@@ -1,43 +1,43 @@
 { config, lib, ... }:
 
+with lib;
+
 let cfg = config.personal; in
 {
-  options.personal.tailscale = lib.mkEnableOption "tailscale";
+  options.personal.tailscale = mkEnableOption "tailscale";
 
-  config = lib.mkIf cfg.tailscale {
+  config =
+    let hosts = {
+      "vps" = {
+        ip = "100.88.115.118";
+      };
+      "dell-xps" = {
+        ip = "100.92.111.117";
+      };
+      "pixel-4a" = {
+        ip = "100.122.46.94";
+      };
+      "desktop" = {
+        ip = "100.93.8.35";
+      };
+      "rasp-pi" = {
+        ip = "100.92.63.87";
+      };
+    }; in
+  mkIf cfg.tailscale {
     services.tailscale.enable = true;
+    networking.firewall.checkReversePath = mkDefault "loose";
 
-    dns.records = [
-      {
-        name = "pixel-4a.vpn";
-        type = "A";
-        data = "100.122.46.94";
-      }
-      {
-        name = "dell-xps.vpn";
-        type = "A";
-        data = "100.92.111.117";
-      }
-      {
-        name = "hp-pavilion";
-        type = "A";
-        data = "100.91.12.120";
-      }
-      {
-        name = "vps.vpn";
-        type = "A";
-        data = "100.88.115.118";
-      }
-      {
-        name = "desktop.vpn";
-        type = "A";
-        data = "100.93.8.35";
-      }
-      {
-        name = "rasp-pi.vpn";
-        type = "A";
-        data = "100.92.63.87";
-      }
-    ];
+    dns.records = attrsets.mapAttrsToList (hostName: values: {
+      name = "${hostName}.vpn";
+      type = "A";
+      data = values.ip;
+    }) hosts;
+
+    networking.extraHosts = builtins.concatStringsSep "\n" (
+      attrsets.mapAttrsToList (
+        hostName: values: "${values.ip} ${hostName}.vpn"
+      ) hosts
+    );
   };
 }
