14bdbf70f0a1497c940947611a6f33b57bba33ee
Ryan Gibb <ryangibb321@gmail.com>
2022-12-22 16:33:21 +0000

    mutt

--- modules/personal/gui/extra.nix
+++ modules/personal/gui/extra.nix
@@ -4,35 +4,42 @@ let cfg = config.personal.gui; in
 {
   options.personal.gui.extra = lib.mkEnableOption "extra";
 
-  config.environment.systemPackages = with pkgs; lib.mkIf cfg.extra [
-    thunderbird
-    element-desktop
-    signal-desktop
-    unstable.zotero
-    unstable.obsidian
-    spotify
-    gimp
-    texlive.combined.scheme-full
-    evince
-    pdfpc
-    calibre
-    transmission
-    transmission-gtk
-    drive
-    libreoffice
-    obs-studio
+  config.environment = lib.mkIf cfg.extra {
+    systemPackages = with pkgs; [
+      thunderbird
+      element-desktop
+      signal-desktop
+      unstable.zotero
+      unstable.obsidian
+      spotify
+      gimp
+      texlive.combined.scheme-full
+      evince
+      pdfpc
+      calibre
+      transmission
+      transmission-gtk
+      drive
+      libreoffice
+      obs-studio
 
-    xournalpp
-    inkscape
+      xournalpp
+      inkscape
 
-    kdenlive
-    tor-browser-bundle-bin
-    zoom-us
-    ffmpeg
-    audio-recorder
-    speechd
-    krop
+      kdenlive
+      tor-browser-bundle-bin
+      zoom-us
+      ffmpeg
+      audio-recorder
+      speechd
+      krop
 
-    bitwarden
-  ];
+      bitwarden
+
+      neomutt
+    ];
+    shellAliases = {
+      mutt = "neomutt";
+    };
+  };
 }
