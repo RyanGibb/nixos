13f60a8b941dd682123f8298a976ca4231def000
Ryan Gibb <ryangibb321@gmail.com>
2023-01-02 15:11:29 +0000

    show thumbnails in wallpaper picker

--- modules/personal/home/wm/scripts/set_selected_wallpaper.sh
+++ modules/personal/home/wm/scripts/set_selected_wallpaper.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR=~/pictures/wallpapers
+WALLPAPER_DIR=$HOME/pictures/wallpapers
 
-WALLPAPER=$(ls $WALLPAPER_DIR | @dmenu@ "$(basename "$(readlink -f $WALLPAPER_DIR/default)")") || exit 1
+WALLPAPER=$(ls $WALLPAPER_DIR | while read A ; do  echo -en "$A\x00icon\x1f$WALLPAPER_DIR/$A\n"; done | rofi -dmenu -p "$(basename "$(readlink -f $WALLPAPER_DIR/default)")") || exit 1
+
+# for wofi:
+#	WALLPAPER=$(ls $WALLPAPER_DIR | while read A ; do echo -en "img:$WALLPAPER_DIR/${A}:text:${A}\n"; done | wofi -d -I -p "$(basename "$(readlink -f $WALLPAPER_DIR/default)")") || exit 1I
+# but very slow...
+# from https://dotfiles.cloudninja.pw/scripts/wofipaper
 
 ln -sf "$WALLPAPER_DIR/$WALLPAPER" $WALLPAPER_DIR/default || exit 1
 
