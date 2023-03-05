79f8f80e2f459c6765fed47cf90284fede5b45d5
Ryan Gibb <ryan@freumh.org>
2023-03-05 13:46:00 +0000

    fix get_free_ws_num.sh to consider duplicate numbers

--- modules/personal/home/wm/scripts/get_free_ws_num.sh
+++ modules/personal/home/wm/scripts/get_free_ws_num.sh
@@ -3,14 +3,14 @@
 ws_nums=($(@wmmsg@ -t get_workspaces \
 	| jq '[.[] | select(.num != -1) | .num ] | sort | .[]'))
 
-# find first non-sequential element of ws_nums from i
-v=1
+# find first element of ws_nums that has a delta between it and it's predecessor greater than 1
+last=0
 for ws_num in ${ws_nums[@]}; do
-	if [ "$ws_num" -ne "$v" ]; then
-		echo "$v"
-		exit 0
+	if [ $(("$ws_num" - "$last")) -gt 1 ]; then
+		break
 	fi
-	((v++))
+	last="$ws_num"
 done
 
-echo "$v"
+echo $(("$last" + 1))
+
