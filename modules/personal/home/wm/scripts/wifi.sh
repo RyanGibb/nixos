dc1875aadca8affce3f8a728ac71655a84a1ed32
Ryan Gibb <ryan@freumh.org>
2023-04-15 18:47:55 +0100

    wifi script: only switch if network selected

--- modules/personal/home/wm/scripts/wifi.sh
+++ modules/personal/home/wm/scripts/wifi.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 mac_addr="$(\
 	nmcli -f BSSID,IN-USE,SSID,CHAN,RATE,SIGNAL,BARS,SECURITY dev wifi list\
 	| tail -n +2\
 	| wofi -d "Select network:"\
 	| awk '{print $1}'
-)"
+)" || exit
 
 nmcli dev wifi connect "$mac_addr"
