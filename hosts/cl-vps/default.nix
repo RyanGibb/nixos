dece38ace79982e3e636619aeef87688f8f3f4ca
Ryan Gibb <ryan@freumh.org>
2023-05-12 15:09:48 +0100

    cl-vps aeon

--- hosts/cl-vps/default.nix
+++ hosts/cl-vps/default.nix
@@ -13,4 +13,24 @@
   };
 
   swapDevices = [ { device = "/var/swap"; size = 2048; } ];
+
+  dns = {
+    zones."example.com" = {
+      soa.serial = lib.mkDefault 2018011626;
+      records = [
+        { name = "@"; type = "A"; data = "127.0.0.1"; }
+      ];
+    };
+  };
+
+  services = {
+    aeon = {
+      enable = true;
+      # TODO make this zonefile derivation a config parameter `services.dns.zonefile`
+      # TODO add module in eilean for aeon
+      zoneFile = "${import "${eilean}/modules/dns/zonefile.nix" { inherit pkgs config lib; zonename = "example.com"; zone = config.dns.zones."example.com"; }}/example.com";
+      logLevel = 1;
+      application = "tund";
+    };
+  };
 }
