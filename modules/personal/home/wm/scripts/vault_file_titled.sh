1c510e93091f9d7ac322229ed87a3880cc88510a
Ryan Gibb <ryan@freumh.org>
2023-05-22 16:18:34 +0100

    remove time from datetime of vault files

--- modules/personal/home/wm/scripts/vault_file_titled.sh
+++ modules/personal/home/wm/scripts/vault_file_titled.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 title="$(zenity --entry --text=Title:)" || exit
-file="$(date '+%Y-%m-%d %H.%M') $title.md"
+file="$(date '+%Y-%m-%d') $title.md"
 cd ~/projects/vault || exit
 vim "$file"
