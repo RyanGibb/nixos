6f1fd5d166b753ba6339e6c94b7bc23a98397b0a
Ryan Gibb <ryan@freumh.org>
2023-08-18 17:52:37 +0100

    update

--- hosts/dagda/default.nix
+++ hosts/dagda/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, nixos-hardware, ... }:
+{ config, pkgs, lib, nixos-hardware, eilean, ... }:
 
 {
   imports = [
@@ -6,7 +6,6 @@
     "${nixos-hardware}/raspberry-pi/4"
   ];
 
-  networking.hostName = "rasp-pi";
   personal = {
     enable = true;
     tailscale = true;
@@ -14,6 +13,26 @@
   };
 
   networking.networkmanager.enable = true;
+
+  dns = {
+    zones.${config.networking.domain} = {
+      soa.serial = lib.mkDefault 2018011626;
+      records = [
+        { name = "@"; type = "A"; data = "127.0.0.1"; }
+      ];
+    };
+  };
+
+  services = {
+    aeon = {
+      enable = true;
+      # TODO make this zonefile derivation a config parameter `services.dns.zonefile`
+      # TODO add module in eilean for aeon
+      zoneFile = "${import "${eilean}/modules/dns/zonefile.nix" { inherit pkgs config lib; zonename = config.networking.domain; zone = config.dns.zones.${config.networking.domain}; }}/${config.networking.domain}";
+      logLevel = 2;
+      application = "tund";
+    };
+  };
 
   services.journald.extraConfig = ''
     SystemMaxUse=4G
