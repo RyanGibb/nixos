c464d7b9f32c14e12974cda59476e5481cc6f1cf
Ryan Gibb <ryan@freumh.org>
2023-10-11 02:06:05 +0100

    eeg

---
+++ hosts/eeg/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, config, lib, eilean, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  personal = {
+    enable = true;
+    machineColour = "green";
+  };
+
+  services.openssh.openFirewall = true;
+
+  swapDevices = [ { device = "/var/swap"; size = 2048; } ];
+
+  security.acme = {
+    defaults.email = "${config.eilean.username}@${config.networking.domain}";
+    acceptTerms = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    xe-guest-utilities
+  ];
+
+  services.hyperbib = {
+    enable = true;
+    domain = "eeg.cl.cam.ac.uk";
+    servicePath = "/";
+  };
+
+  nix.settings.require-sigs = false;
+}
