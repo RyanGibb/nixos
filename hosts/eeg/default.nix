704fd160f69f29e0bcd6622d256ba5287247a959
Ryan Gibb <ryan@freumh.org>
2023-10-11 21:24:49 +0100

    eeg hyperbib add raven authentication

--- hosts/eeg/default.nix
+++ hosts/eeg/default.nix
@@ -1,5 +1,6 @@
 { pkgs, config, lib, eilean, ... }:
 
+let domain = "eeg.cl.cam.ac.uk"; in
 {
   imports = [
     ./hardware-configuration.nix
@@ -25,10 +26,54 @@
 
   services.hyperbib = {
     enable = true;
-    domain = "eeg.cl.cam.ac.uk";
+    domain = domain;
     # servicePath = "/bib/";
     # proxyPath = "/";
   };
+
+  services.nginx.enable = lib.mkForce false;
+  services.httpd = {
+    enable = true;
+    extraModules = let
+      mod_ucam_webauth = pkgs.callPackage ./mod_ucam_webauth.nix { };
+    in [ {
+      name = "ucam_webauth";
+      path = "${mod_ucam_webauth}/modules/mod_ucam_webauth.so";
+    } ];
+
+    virtualHosts."${domain}" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."${config.services.hyperbib.proxyPath}" = {
+        proxyPass = "http://127.0.0.1:${builtins.toString config.services.hyperbib.port}/";
+      };
+      extraConfig = let
+        keyfile = pkgs.writeTextFile {
+          name = "raven-rsa-key";
+          destination = "/pubkey2";
+          text = ''
+            -----BEGIN RSA PUBLIC KEY-----
+            MIGJAoGBAL/2pwBbVcJKTRF8B+K6W9Oi4xkoPiOb32te0whw7Zuf7cTFCk5tvBa6
+            CI7wM0R99LtvNLFmoantTps92LjF9fvrCBYZDqpaLnk5clXShKKqt3do4SykqYkq
+            66kpc42jZ58C3omR0dUfQ7o7yTktVqnrDjLVb9P+vLhAfuSFHFa1AgMBAAE=
+            -----END RSA PUBLIC KEY-----
+          '';
+        };
+      in ''
+        AAKeyDir ${keyfile}
+        AACookieKey file:/dev/urandom
+        <Location "/bib/">
+          AuthType Ucam-WebAuth
+          Require valid-user
+        </Location>
+      '';
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80   # HTTP
+    443  # HTTPS
+  ];
 
   nix.settings.require-sigs = false;
 }
---
+++ hosts/eeg/mod_ucam_webauth.nix
@@ -0,0 +1,24 @@
+{ lib, stdenv, fetchFromGitHub, apacheHttpd, openssl }:
+
+stdenv.mkDerivation rec {
+  pname = "mod_ucam_webauth";
+  version = "2.0.5";
+
+  src = fetchFromGitHub {
+    owner = "cambridgeuniversity";
+    repo = "mod_ucam_webauth";
+    rev = "6a246d5299171713c8680ef37c6ea6545bac948a";
+    hash = "sha256-NbwHRy0SYXvpVBzJGhgI3IAAY+uVr9yRvFfMACKK4MU=";
+  };
+
+  buildInputs = [  openssl.dev ];
+
+  buildPhase = ''
+    make APXS=${apacheHttpd.dev}/bin/apxs
+  '';
+
+  installPhase = ''
+    mkdir -p $out/modules/
+    cp ./.libs/mod_ucam_webauth.so $out/modules/
+  '';
+}
