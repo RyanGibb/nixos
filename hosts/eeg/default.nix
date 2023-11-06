d6c00515709239569fc2bc3d3fe55bcbc4441bff
Ryan Gibb <ryan@freumh.org>
2023-11-06 10:35:53 +0000

    eeg matrix server

--- hosts/eeg/default.nix
+++ hosts/eeg/default.nix
@@ -60,6 +60,13 @@ let domain = "eeg.cl.cam.ac.uk"; in
             -----END RSA PUBLIC KEY-----
           '';
         };
+        matrixServerConfig = pkgs.writeText "matrix-server-config.json" (builtins.toJSON {
+          "m.server" = "${domain}:443";
+        });
+        matrixClientConfig = pkgs.writeText "matrix-server-config.json" (builtins.toJSON {
+          "m.homeserver" =  { "base_url" = "https://${domain}"; };
+          "m.identity_server" =  { "base_url" = "https://vector.im"; };
+        });
       in ''
         AAKeyDir ${keyfile}
         AACookieKey file:/dev/urandom
@@ -67,8 +74,98 @@ let domain = "eeg.cl.cam.ac.uk"; in
           AuthType Ucam-WebAuth
           Require valid-user
         </Location>
+
+        SSLEngine on
+        ServerName ${domain}
+
+        ### Matrix config
+
+        RequestHeader set "X-Forwarded-Proto" expr=%{REQUEST_SCHEME}
+        AllowEncodedSlashes NoDecode
+        ProxyPreserveHost on
+        ProxyPass /_matrix http://127.0.0.1:8008/_matrix nocanon
+        ProxyPassReverse /_matrix http://127.0.0.1:8008/_matrix
+        ProxyPass /_synapse/client http://127.0.0.1:8008/_synapse/client nocanon
+        ProxyPassReverse /_synapse/client http://127.0.0.1:8008/_synapse/client
+
+        Alias /.well-known/matrix/server "${matrixServerConfig}"
+        Alias /.well-known/matrix/client "${matrixClientConfig}"
       '';
     };
+  };
+
+  services.postgresql.enable = true;
+  services.postgresql.package = pkgs.postgresql_13;
+  services.postgresql.initialScript = pkgs.writeText "synapse-init.sql" ''
+    CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+    CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+      TEMPLATE template0
+      LC_COLLATE = "C"
+      LC_CTYPE = "C";
+  '';
+
+  services.matrix-synapse = {
+    enable = true;
+    settings = lib.mkMerge [
+      {
+        server_name = domain;
+        enable_registration = true;
+        registration_requires_token = true;
+        registration_shared_secret_path = "/secrets/matrix-shared-secret";
+        listeners = [
+          {
+            port = 8008;
+            bind_addresses = [ "::1" "127.0.0.1" ];
+            type = "http";
+            tls = false;
+            x_forwarded = true;
+            resources = [
+              {
+                names = [ "client" "federation" ];
+                compress = false;
+              }
+            ];
+          }
+        ];
+        max_upload_size = "100M";
+      }
+    ];
+    extraConfigFiles = [
+      # generate keys with
+      #   sudo nix shell nixpkgs#openssl nixpkgs#shibboleth-sp -c sh -c '`nix eval --raw nixpkgs#shibboleth-sp`/etc/shibboleth/keygen.sh -h matrix.eeg.cl.cam.ac.uk -o /secrets/matrix-shibboleth/'
+      #   chown -R matrix-synapse /secrets/matrix-shibboleth/
+      (pkgs.writeText "shibboleth.yml" ''
+        saml2_config:
+          sp_config:
+            metadata:
+              remote:
+                - url: https://shib.raven.cam.ac.uk/shibboleth
+            description: ["Energy and Environment Group Computer Lab Matrix Server", "en"]
+            name: ["EEG CL Matrix Server", "en"]
+            key_file: /secrets/matrix-shibboleth/sp-key.pem
+            cert_file: /secrets/matrix-shibboleth/sp-cert.pem
+            encryption_keypairs:
+              - key_file: /secrets/matrix-shibboleth/sp-key.pem
+                cert_file: /secrets/matrix-shibboleth/sp-cert.pem
+            attribute_map_dir: ${pkgs.writeTextDir "map.py" ''
+              MAP = {
+                  "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:uri",
+                  "fro": {
+                      'urn:oid:0.9.2342.19200300.100.1.1': 'uid',
+                      'urn:oid:0.9.2342.19200300.100.1.3': 'email',
+                      'urn:oid:2.16.840.1.113730.3.1.241': 'displayName',
+                  },
+                  "to": {
+                      'uid': 'urn:oid:0.9.2342.19200300.100.1.1',
+                      'email': 'urn:oid:0.9.2342.19200300.100.1.3',
+                      'displayName': 'urn:oid:2.16.840.1.113730.3.1.241',
+                  },
+              }
+            ''}
+        password_config:
+          enabled: false
+      '')
+    ];
   };
 
   networking.firewall.allowedTCPPorts = [
