8db0f60264504355c1fed0313857d57c36a03081
Ryan Gibb <ryan@freumh.org>
2024-01-11 23:34:08 +0000

    dict

---
+++ modules/personal/dict.nix
@@ -0,0 +1,17 @@
+{ pkgs, config, lib, ... }:
+
+let cfg = config.personal; in
+{
+  options.personal.dict = lib.mkOption {
+    type = types.bool;
+    default = true;
+  };
+
+  config = lib.mkIf cfg.dict {
+    services.dictd.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      dict
+    ];
+  };
+}
