2386b09841b9a37ae31877eb84fc328bca402249
Ryan Gibb <ryan@freumh.org>
2024-01-15 10:43:24 +0000

    elephant

---
+++ hosts/elephant/default.nix
@@ -0,0 +1,57 @@
+{ pkgs, config, lib, eilean, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  personal = {
+    enable = true;
+    tailscale = true;
+    machineColour = "blue";
+  };
+
+  environment.systemPackages = with pkgs; [
+    smartmontools
+    powertop
+    hdparm
+  ];
+
+  boot.loader.grub = {
+    enable = true;
+    default = "saved";
+    device = "nodev";
+    efiSupport = true;
+  };
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+
+  swapDevices = [ { device = "/var/swap"; size = 16384; } ];
+
+  eilean = {
+    publicInterface = "enp1s0";
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
+  services.minidlna = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      media_dir = [ "/mnt/hdd" ];
+      notify_interval = 60;
+      inofity = true;
+    };
+  };
+
+  powerManagement.powertop.enable = true;
+}
