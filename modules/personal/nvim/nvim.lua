d671b0de65a0eb505fc9290958f5837263ee55ca
Ryan Gibb <ryan@freumh.org>
2024-02-13 21:16:22 +0000

    nvim session management

--- modules/personal/nvim/nvim.lua
+++ modules/personal/nvim/nvim.lua
@@ -222,3 +222,60 @@ cmp.setup {
 	},
 }
 
+
+local session_dir = vim.fn.stdpath('data') .. '/sessions/'
+
+local function ensure_dir_exists(dir)
+	if vim.fn.isdirectory(dir) == 0 then
+		vim.fn.mkdir(dir, 'p')
+	end
+end
+
+local function sanitize_path(path)
+	return path:gsub('[\\/]+', '_'):gsub('^_*', ''):gsub('_*$', '')
+end
+
+local function session_file_name(name)
+	local cwd = vim.fn.getcwd()
+	local sanitized_cwd = sanitize_path(cwd)
+	if name and #name > 0 then
+		return session_dir .. 'session:' .. sanitized_cwd .. ':' .. sanitize_path(name) .. '.vim'
+	else
+		return session_dir .. 'session:' .. sanitized_cwd .. '.vim'
+	end
+end
+
+local function save_session(args)
+	ensure_dir_exists(session_dir)
+	local session_file = session_file_name(args.args)
+	vim.cmd('mksession! ' .. vim.fn.fnameescape(session_file))
+end
+
+local function load_session(args)
+	local session_file = session_file_name(args.args)
+	if vim.fn.filereadable(session_file) == 1 then
+		vim.cmd('source ' .. vim.fn.fnameescape(session_file))
+	else
+		print("No session file found for " .. (args.args == "" and "this directory" or args.args))
+	end
+end
+
+local function session_completion(arg_lead, cmd_line, cursor_pos)
+	local files = vim.fn.globpath(session_dir, 'session:' .. sanitize_path(vim.fn.getcwd()) .. ':*', 0, 1)
+	local sessions = {}
+	for _, file in ipairs(files) do
+		local session = file:match('session:' .. sanitize_path(vim.fn.getcwd()) .. ':(.+)%.vim$')
+		if session and session:find(arg_lead, 1, true) == 1 then
+			table.insert(sessions, session)
+		end
+	end
+	return sessions
+end
+
+vim.api.nvim_create_autocmd("VimLeave", {
+	pattern = '*',
+	callback = function() save_session({args = ""}) end,
+})
+
+vim.api.nvim_create_user_command('SaveSession', save_session, { nargs = '?', complete = session_completion })
+vim.api.nvim_create_user_command('LoadSession', load_session, { nargs = '?', complete = session_completion })
