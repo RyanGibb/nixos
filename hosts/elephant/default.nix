b7ff6749e6d686095ebe8406777470d866783d28
Ryan Gibb <ryan@freumh.org>
2024-02-22 20:49:16 +0000

    restic backblaze backup

--- hosts/elephant/default.nix
+++ hosts/elephant/default.nix
@@ -17,6 +17,7 @@
     smartmontools
     powertop
     hdparm
+    restic
   ];
 
   eilean = {
@@ -30,5 +31,25 @@
       sudo ${pkgs.hdparm}/sbin/hdparm -S 242 /dev/disk/by-id/ata-ST16000NM001G-2KK103_ZL28CDKQ
       sudo ${pkgs.hdparm}/sbin/hdparm -S 242 /dev/disk/by-id/ata-TOSHIBA_MG08ACA16TE_83E0A00UFVGG
     '';
+  };
+
+  services.restic.backups.daily = {
+    environmentFile = "${config.custom.secretsDir}/restic.env";
+    repositoryFile = "${config.custom.secretsDir}/restic-repo";
+    passwordFile = "${config.custom.secretsDir}/restic-password";
+    initialize = true;
+    paths = [
+      "/tank/family/mp4/"
+      "/tank/family/other/"
+      "/tank/photos/"
+    ];
+    timerConfig = {
+      OnCalendar = "03:00";
+    };
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 4"
+      "--keep-yearly 10"
+    ];
   };
 }
