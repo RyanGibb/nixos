885a84d706789bfc01279d279531c8658ca40094
Ryan Gibb <ryan@freumh.org>
2024-05-24 23:45:05 +0100

    hmrc-paye-tool

---
+++ pkgs/hmrc-paye-tool.nix
@@ -0,0 +1,78 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+let
+  version = "24.1.24086.542";
+
+  installer = stdenv.mkDerivation rec {
+    name = "hmrc-paye-tools-installer";
+    inherit version;
+
+    src = fetchurl {
+      url = "https://www.gov.uk/government/uploads/uploaded/hmrc/payetools-rti-${version}-linux.zip";
+      hash = "sha256-QOW6Loqg001AcqWX/TOH6wvI2uAY4qNyFvQzCVEe8VU=";
+    };
+
+    dontUnpack = true;
+    dontFixup = true;
+
+    buildInputs = [ unzip ];
+
+    buildPhase = ''
+      mkdir -p $out/bin
+      unzip $src -d $out/bin
+      chmod +x $out/bin/*
+    '';
+  };
+
+  installerFHS = buildFHSEnv {
+    name = "hmrc-paye-tools-installer-fhs";
+    targetPkgs = pkgs: [ pkgs.glibc ];
+    runScript = "${installer}/bin/payetools-rti-${version}-linux";
+  };
+
+  package = stdenv.mkDerivation {
+    name = "hmrc-paye-tools";
+    inherit version;
+
+    dontUnpack = true;
+
+    buildPhase = ''
+      ${installerFHS}/bin/hmrc-paye-tools-installer-fhs --mode unattended --prefix prefix --check_for_updates 0
+      mv prefix $out
+    '';
+  };
+in buildFHSUserEnv {
+  name = "hmrc-paye-tools-fhs";
+
+  targetPkgs = pkgs: (with pkgs; [
+    glibc
+    zlib
+    fontconfig
+    glib
+    libpng
+    freetype
+    xorg.libSM
+    xorg.libICE
+    xorg.libXrender
+    xorg.libXext
+    xorg.libX11
+    xorg.libXt
+    xorg.libXau
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
+    sqlite
+    libGL
+    gtk2
+  ]);
+  runScript = "${package}/rti.linux";
+
+  meta = with lib; {
+    description = "HMRC Basic PAYE Tools";
+    homepage = "https://www.gov.uk/basic-paye-tools";
+    license = licenses.unfree;
+    platforms = platforms.linux;
+  };
+}
