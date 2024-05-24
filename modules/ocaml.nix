0a08e7fbf03c2681e0a7372fc807497163f689ab
Ryan Gibb <ryan@freumh.org>
2024-05-24 23:44:40 +0100

    ocaml remove depexts
    https://github.com/RyanGibb/opam/pull/1

--- modules/ocaml.nix
+++ modules/ocaml.nix
@@ -5,23 +5,6 @@ in {
   options.custom.ocaml = lib.mkEnableOption "ocaml";
 
   config = lib.mkIf cfg.ocaml {
-    environment.systemPackages = with pkgs; [
-      ocaml
-      opam
-      dune_3
-      ocamlPackages.utop
-      pkg-config
-      gcc
-      gmp
-      bintools-unwrapped
-      libseccomp
-      opam
-      capnproto
-      gmp
-      sqlite
-    ];
-
-    programs.zsh.interactiveShellInit = "eval $(opam env)";
-    programs.bash.shellInit = "eval $(opam env)";
+    environment.systemPackages = with pkgs; [ opam pkg-config ];
   };
 }
