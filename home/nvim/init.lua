46c54a97fdc3268f96f9ecd9771317b552d14bec
Ryan Gibb <ryan@freumh.org>
2024-07-25 12:09:26 +0100

    telescope

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -150,24 +150,33 @@ vim.cmd([[
 
 -- telescope
 
+require('telescope').load_extension('fzf')
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-Down>"] = require('telescope.actions').cycle_history_next,
+        ["<C-Up>"] = require('telescope.actions').cycle_history_prev,
+      },
+    },
+  }
+}
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, {})
 vim.keymap.set('n', '<leader>fv', require('telescope.builtin').git_files, {})
 vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers({ sort_mru = true }) end, {})
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, {})
-vim.keymap.set('n', '<leader>fc', require('telescope.builtin').command_history, {})
+vim.keymap.set('n', '<leader>fq', require('telescope.builtin').command_history, {})
 vim.keymap.set('n', '<leader>fs', require('telescope.builtin').search_history, {})
 vim.keymap.set('n', '<leader>fj', require('telescope.builtin').jumplist, {})
 vim.keymap.set('n', '<leader>fm', require('telescope.builtin').marks, {})
+vim.keymap.set('n', '<leader>fx', require('telescope.builtin').diagnostics, {})
+vim.keymap.set('n', '<leader>fy', require('telescope.builtin').registers, {})
 vim.keymap.set('n', '<leader>fr', require('telescope.builtin').lsp_references, {})
 vim.keymap.set('n', '<leader>fS', require('telescope.builtin').lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>fc', require('telescope.builtin').lsp_incoming_calls, {})
 vim.keymap.set('n', '<leader>fo', require('telescope.builtin').lsp_outgoing_calls, {})
 vim.keymap.set('n', '<leader>fi', require('telescope.builtin').lsp_implementations, {})
-vim.keymap.set('n', '<leader>fx', require('telescope.builtin').diagnostics, {})
-vim.keymap.set('n', '<leader>fy', require('telescope.builtin').registers, {})
-
-require('telescope').load_extension('fzf')
 
 -- trouble
 
