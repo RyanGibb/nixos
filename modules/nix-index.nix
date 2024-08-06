75b2ea059185621244d8901c9bc2c8e8b7d90922
Ryan Gibb <ryan@freumh.org>
2024-08-06 10:16:13 +0100

    fix nix-index

--- modules/nix-index.nix
+++ modules/nix-index.nix
@@ -5,8 +5,10 @@ in {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ pkgs.nix-index ];
     programs.command-not-found.enable = false;
-    programs.zsh.interactiveShellInit = ''
-      source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
-    '';
+    home-manager.users.${config.custom.username} = { ... }: {
+      programs.zsh.initExtra = ''
+        source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
+      '';
+    };
   };
 }
