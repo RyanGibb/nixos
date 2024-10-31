311781662a5dcf0c7f0446fbb3e1b95ece5d7b4d
Ryan Gibb <ryan@freumh.org>
2024-10-31 16:51:13 +0000

    update

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -14,7 +14,7 @@
 (require 'ledger-mode)
 (setq ledger-post-amount-alignment-column 52)
 (setq ledger-default-date-format ledger-iso-date-format)
-(setq ledger-post-account-alignment-column 2)
+(setq ledger-post-account-alignment-column 4)
 (setq ledger-reconcile-default-commodity '£)
 
 (setq-default truncate-lines t)
