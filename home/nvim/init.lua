040aa6af6b02bb294fb510bc1313da154c63bab1
Ryan Gibb <ryan@freumh.org>
2024-11-04 11:51:02 +0000

    oops

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -469,6 +469,12 @@ vim.g.ledger_extra_options = '--pedantic'
 vim.g.ledger_align_at = 50
 vim.g.ledger_accounts_cmd = 'ledger accounts --add-budget'
 
+vim.g.ledger_date_format = '%Y-%m-%d'
+
+vim.cmd([[
+autocmd FileType ledger nnoremap <buffer> <leader>e :call ledger#entry()<CR>
+]])
+
 -- session management
 
 local session_dir = vim.fn.stdpath('data') .. '/sessions/'
