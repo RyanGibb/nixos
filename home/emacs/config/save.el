692907fb4b987640a8754164ae71ed0a0c94596e
Ryan Gibb <ryan@freumh.org>
2024-11-12 15:43:31 +0000

    update

--- home/emacs/config/save.el
+++ home/emacs/config/save.el
@@ -8,7 +8,7 @@
 (setq history-length 10000)
 (setq savehist-save-minibuffer-history t)
 
-(desktop-save-mode 1)
+(desktop-save-mode 0)
 (setq desktop-dirname "~/.emacs.d/desktop/")
 (setq desktop-save t)
 (add-hook 'emacs-startup-hook 'desktop-read)
