d051bbc54be2c0d428e54b9572a62930be24e1b1
Ryan Gibb <ryan@freumh.org>
2024-11-13 15:50:14 +0000

    use treesitter to add text objects for functions

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -235,6 +235,25 @@ in {
               vim.opt.foldenable = false
             '';
           }
+          {
+            plugin = nvim-treesitter-textobjects;
+            type = "lua";
+            config = ''
+              require'nvim-treesitter.configs'.setup {
+                textobjects = {
+                  select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                      ["af"] = "@function.outer",
+                      ["if"] = "@function.inner",
+                    },
+                    include_surrounding_whitespace = true,
+                  },
+                },
+              }
+            '';
+          }
 
           {
             plugin = vimtex;
