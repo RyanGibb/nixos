19b224395755a08d01f7b956a5712eef781f16be
Ryan Gibb <ryan@freumh.org>
2024-12-14 15:47:51 +0000

    zork

---
+++ hosts/zork/default.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  custom = {
+    enable = true;
+    tailscale = true;
+    homeManager.enable = true;
+  };
+
+  networking.networkmanager.enable = true;
+  services.openssh.openFirewall = true;
+
+  home-manager.users.${config.custom.username}.config.custom.machineColour = "red";
+}
