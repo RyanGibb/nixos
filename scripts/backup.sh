adc7a935cb73a7c3bbc1f539c7751ae2f3ef0ac1
Ryan Gibb <ryan@freumh.org>
2025-01-16 12:35:50 +0000

    removed outdated scripts

--- scripts/backup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-rsync -va --exclude={".cache", ".local/share/Steam/"} ~/ /run/media/ryan/external-hdd/home/
--- scripts/obsidian_sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cd /storage/emulated/0/Documents/vault
-git pull &&
-git add . &&
-git commit -m "phone $(date '+%Y-%m-%d %H.%M')" &&
-git push
