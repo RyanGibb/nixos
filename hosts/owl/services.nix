abe26c6f9f4f1b652ee5a864c7e68de837a74810
Ryan Gibb <ryan@freumh.org>
2025-04-29 21:42:43 +0000

    update

--- hosts/owl/services.nix
+++ hosts/owl/services.nix
@@ -283,17 +283,11 @@ in
     enableACME = true;
     root = "/var/www/meands.org/_site";
     extraConfig = ''
-      add_header Content-Security-Policy "
-            default-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
-            base-uri 'self';
-            frame-src 'self';
-            frame-ancestors 'self';
-            form-action 'self';
-            style-src 'self' https://cdn.jsdelivr.net;
-            font-src 'self' https://fonts.gstatic.com;
-            script-src 'self' https://cdn.jsdelivr.net;
-            "
-            always;
+      add_header Strict-Transport-Security max-age=31536000 always;
+        add_header X-Frame-Options SAMEORIGIN always;
+        add_header X-Content-Type-Options nosniff always;
+        add_header Referrer-Policy 'same-origin';
+        add_header Content-Security-Policy "default-src 'self' 'unsafe-inline' 'unsafe-eval' blob:; base-uri 'self'; frame-src 'self'; frame-ancestors 'self'; form-action 'self'; style-src 'self' https://cdn.jsdelivr.net; font-src 'self' https://fonts.gstatic.com; script-src 'self' https://cdn.jsdelivr.net;"always;
     '';
   };
 
