d0a371dd01c74cbefe814bd7afa97c3d1bf82429
Ryan Gibb <ryan@freumh.org>
2025-04-30 13:41:17 +0100

    update

--- hosts/gecko/default.nix
+++ hosts/gecko/default.nix
@@ -238,4 +238,22 @@
       intel-compute-runtime
     ];
   };
+
+  # camera
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="04cb", ENV{ID_MODEL_ID}=="02dd", TAG+="systemd", SYMLINK+="fujifilmxt3"
+    ACTION=="add", SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="04cb", ENV{ID_MODEL_ID}=="02dd", TAG+="systemd", ENV{SYSTEMD_USER_WANTS}="mount-camera.service"
+  '';
+  systemd.user.services.mount-camera = {
+    unitConfig = {
+      BindsTo = "dev-fujifilmxt3.device";
+      After = "dev-fujifilmxt3.device";
+    };
+    serviceConfig = {
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /run/media/ryan";
+      ExecStart = "${pkgs.gphoto2fs}/bin/gphotofs /run/media/ryan -f";
+      Restart = "no";
+      KillMode = "process";
+    };
+  };
 }
