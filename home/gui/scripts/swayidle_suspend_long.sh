966db6b16cd987ebe5f8938e6b49e714372ced75
Ryan Gibb <ryan@freumh.org>
2025-08-30 17:51:01 +0100

    update

--- home/gui/scripts/swayidle_suspend_long.sh
+++ home/gui/scripts/swayidle_suspend_long.sh
@@ -4,9 +4,9 @@ pkill -x swayidle
 
 swayidle -w\
 	lock '@locker@'\
-	timeout 300 "notify-send 'going to sleep soon!' -t 300000"\
+	timeout 3300 "notify-send 'going to sleep soon!' -t 300000"\
 	timeout 3600 '@wmmsg@ "output * dpms off"'\
 		resume '@wmmsg@ "output * dpms on"'\
 	timeout 3900 'loginctl lock-session'\
-	timeout 7200 'systemctl suspend-then-hibernate'\
+	timeout 7200 'systemctl suspend'\
 	before-sleep 'playerctl -a pause; loginctl lock-session'
