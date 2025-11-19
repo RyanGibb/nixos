44c057f0ffcecd22b7e98d793666a82a2b48c0c4
Ryan Gibb <ryan@freumh.org>
2025-11-19 14:13:44 +0000

    update

--- flake.nix
+++ flake.nix
@@ -66,6 +66,7 @@
           #  (_: { patches = [ ./pkgs/package.patch ]; });
           opam = final.overlay-unstable.opam;
           immich = final.overlay-unstable.immich;
+          mautrix-whatsapp = prev.callPackage ./pkgs/mautrix-whatsapp.nix { };
         })
         inputs.nur.overlays.default
         inputs.nix-doom-emacs.overlays.default
