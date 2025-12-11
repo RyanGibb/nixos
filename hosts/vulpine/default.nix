3d1da151d13de9f3e88f3229e0706599901b8e35
Ryan Gibb <ryan@freumh.org>
2025-12-11 22:29:04 +0000

    Revert "Simplify to use NVIDIA driver 535 from nixpkgs"
    This reverts commit 934c15aecd05db9f40e74f0c19f1f00f7bc32698.

--- hosts/vulpine/default.nix
+++ hosts/vulpine/default.nix
@@ -85,8 +85,16 @@
 
   services.avahi.enable = true;
 
-  # Use NVIDIA driver 535 (legacy stable)
-  hardware.nvidia.package = config.boot.kernelPackages.nvidia_x11_legacy535;
+  # Use kernel and nvidia driver from nixpkgs-nvidia (6.12.41 with nvidia 575)
+  boot.kernelPackages = let nvidia-nixpkgs = import inputs.nixpkgs-nvidia {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    config.allowUnfree = true;
+  }; in nvidia-nixpkgs.linuxPackages_6_12;
+
+  hardware.nvidia.package = (import inputs.nixpkgs-nvidia {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    config.allowUnfree = true;
+  }).linuxPackages_6_12.nvidia_x11_latest;
 
   specialisation.nvidia.configuration = {
     services.xserver.videoDrivers = [ "nvidia" ];
