eb27846cf0483308553628311ae6b8c62da3363c
Ryan Gibb <ryan@freumh.org>
2026-02-24 12:57:40 +0000

    update

--- home/emacs/doom/init.el
+++ home/emacs/doom/init.el
@@ -123,7 +123,7 @@
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex +lsp)       ; writing papers in Emacs has never been so fun
-       (lean +lsp)              ; for folks with too much to prove
+       lean              ; for folks with too much to prove
        ledger            ; be audit you can be
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
