e5e4230425cedc48ff2186880ef8b542dd409b44
Ryan Gibb <ryan@freumh.org>
2026-04-21 11:15:07 +0100

    update

--- hosts/gecko/default.nix
+++ hosts/gecko/default.nix
@@ -424,4 +424,19 @@
   };
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  fileSystems."/mnt/elephant" = {
+    device = "//100.64.0.9/tank";
+    fsType = "cifs";
+    options = [
+      "credentials=/etc/samba-credentials"
+      "uid=1000"
+      "gid=100"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "noauto"
+    ];
+  };
 }
