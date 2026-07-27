ed58cfd477a25db3e51a3b2c12b28a44d69c6447
Ryan Gibb <ryan@freumh.org>
2026-07-27 14:06:36 +0100

    update

--- hosts/gecko/default.nix
+++ hosts/gecko/default.nix
@@ -456,7 +456,7 @@
     options = [
       "x-systemd.automount"
       "noauto"
-      "x-systemd.idle-timeout=60"
+      "x-systemd.idle-timeout=1800"
       "x-systemd.device-timeout=5s"
       "x-systemd.mount-timeout=5s"
     ];
